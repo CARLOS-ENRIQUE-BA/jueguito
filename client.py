6904621b9cf636fa266e54180df5309405965b2f
CARLOS-ENRIQUE-BA <carlos_enbagui@hotmail.com>
2024-04-20 19:57:38 -0600

    first commit

---
+++ client.py
@@ -0,0 +1,83 @@
+import asyncio
+import websockets
+import pygame
+from pygame.locals import *
+import json
+
+server_ip = '34.226.111.91'
+server_port = 9009
+
+pygame.init()
+screen = pygame.display.set_mode((850, 530))
+clock = pygame.time.Clock()
+
+font = pygame.font.Font(None, 20)
+
+# Definir la posición inicial del jugador en función del lado de la pantalla
+if server_ip == '0.0.0.0':
+    jugador_lado = 'izquierdo'
+else:
+    jugador_lado = 'derecho'
+
+if jugador_lado == 'izquierdo':
+    estado_jugador = {'x': 50, 'y': 300, 'ready': False}
+else:
+    estado_jugador = {'x': 750, 'y': 300, 'ready': False}
+
+estado_global = {}
+pelotas = [] 
+
+async def actualizar_estado(websocket):
+    global estado_global, pelotas
+
+    data = await websocket.recv()
+    if data:
+        estado = json.loads(data)
+        estado_global = estado['estado_global']
+        pelotas = estado.get('pelotas', [])
+
+async def enviar_movimiento(websocket):
+    await websocket.send(json.dumps(estado_jugador))
+    await actualizar_estado(websocket)
+
+async def main():
+    async with websockets.connect(f"ws://{server_ip}:{server_port}") as websocket:
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    running = False
+                elif event.type == KEYDOWN and event.key == K_c:  
+                    estado_jugador['ready'] = not estado_jugador['ready']
+                    await enviar_movimiento(websocket)
+            
+            keys = pygame.key.get_pressed()
+            
+            # Ajustando la velocidad de movimiento del jugador
+            if keys[K_UP] and estado_jugador['y'] > 0:
+                estado_jugador['y'] -= 20  # Doble de rápido
+            if keys[K_DOWN] and estado_jugador['y'] < 510:
+                estado_jugador['y'] += 20  # Doble de rápido
+
+            await enviar_movimiento(websocket)
+
+            screen.fill((0, 128, 0))  
+
+            for id_jugador, pos in estado_global.items():
+                pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(pos['x'], pos['y'], 20, 120)) 
+                
+            for pelota_info in pelotas:
+                pelota_info['x'] -= 50 # Triple de rápido
+                pygame.draw.circle(screen, (255, 0, 0), (int(pelota_info['x']), int(pelota_info['y'])), 10) 
+
+            mensaje_texto = font.render("Todos los jugadores opriman c para comenzar", True, (255, 255, 255))
+            screen.blit(mensaje_texto, ((screen.get_width() - mensaje_texto.get_width()) // 2, 10))
+
+            pygame.display.update()
+
+            clock.tick(60)
+
+    pygame.quit()
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
---
+++ server.py
@@ -0,0 +1,146 @@
+import asyncio
+import websockets
+import json
+import random
+
+server_ip = '0.0.0.0'
+server_port = 9009
+
+clientes = set()
+
+estado_global = {}
+
+contador_jugadores = 0
+
+pelotas = []
+
+tiempo_restante = 0
+
+puntos_jugador_izquierdo = 0
+puntos_jugador_derecho = 0
+
+def verificar_todos_listos():
+    global todos_listos
+    todos_listos = all(jugador['ready'] for jugador in estado_global.values())
+
+async def generar_pelotas():
+    intervalo = 1
+
+    while True:
+        if clientes and todos_listos and tiempo_restante > 0:
+            if not pelotas:
+                pelota = {
+                    'x': 425,
+                    'y': 265,
+                    'velocidad_x': random.uniform(-6, -15),  # Velocidad reducida
+                    'velocidad_y': random.choice([-1, 1])  
+                }
+                pelotas.append(pelota)
+        
+        await asyncio.sleep(intervalo)
+
+async def manejar_cliente(websocket, path):
+    global contador_jugadores, tiempo_restante
+    id_jugador = contador_jugadores
+    contador_jugadores += 1
+    estado_global[id_jugador] = {'x': 400, 'y': 300, 'ready': False}
+    clientes.add(websocket)
+    print(f"Player {id_jugador} se ha unido al servidor.")
+
+    try:
+        while True:
+            datos = await websocket.recv()
+            movimiento = json.loads(datos)
+            estado_global[id_jugador] = movimiento
+            
+            if not movimiento['ready'] and tiempo_restante > 0:
+                tiempo_restante = 0
+                pelotas.clear()
+
+    except websockets.ConnectionClosed:
+        print(f"La conexión con el cliente {id_jugador} ha sido cerrada inesperadamente.")
+    finally:
+        clientes.remove(websocket)
+        if id_jugador in estado_global:
+            del estado_global[id_jugador]
+            print(f"Player {id_jugador} ha sido eliminado")
+
+def verificar_colisiones():
+    global tiempo_restante, puntos_jugador_izquierdo, puntos_jugador_derecho
+    for id_jugador, jugador in estado_global.items():
+        x_jugador = jugador['x']
+        y_jugador = jugador['y']
+        
+        for pelota in pelotas:
+            x_pelota = pelota['x']
+            y_pelota = pelota['y']
+            
+            # Verificar colisión con el jugador
+            if (x_jugador < x_pelota + 20 and x_jugador + 87 > x_pelota and
+                y_jugador < y_pelota + 20 and y_jugador + 120 > y_pelota):
+                pelota['velocidad_x'] *= -1  
+                pelota['velocidad_y'] *= random.choice([-1, 1])  
+
+            # Verificar colisión con los bordes laterales de la pantalla
+            if x_pelota < 0:
+                puntos_jugador_derecho += 1
+                pelota['x'] = 425  # Reseteamos la posición de la pelota al centro
+                pelota['y'] = 265
+                pelota['velocidad_x'] *= -1
+            elif x_pelota > 900:
+                puntos_jugador_izquierdo += 1
+                pelota['x'] = 425
+                pelota['y'] = 265
+                pelota['velocidad_x'] *= -1
+
+            # Verificar colisión con los bordes superior e inferior de la pantalla
+            if y_pelota < 0 or y_pelota > 530:
+                pelota['velocidad_y'] *= -1
+
+async def actualizar_estado():
+    global tiempo_restante
+
+    while True:
+        verificar_todos_listos()
+
+        if todos_listos and tiempo_restante == 0:
+            tiempo_restante = 120
+            asyncio.create_task(generar_pelotas())
+        
+        if todos_listos and tiempo_restante > 0:
+            tiempo_restante -= 0.1
+            verificar_colisiones()
+
+            if tiempo_restante <= 0:
+                tiempo_restante = 0
+                pelotas.clear()
+        
+        for pelota in pelotas:
+            pelota['x'] += pelota['velocidad_x']
+            pelota['y'] += pelota['velocidad_y']
+
+        pelotas[:] = [pelota for pelota in pelotas if pelota['x'] > 0]
+
+        estado = {
+            'estado_global': estado_global,
+            'pelotas': pelotas,
+            'tiempo_restante': tiempo_restante,
+            'puntos_jugador_izquierdo': puntos_jugador_izquierdo,
+            'puntos_jugador_derecho': puntos_jugador_derecho
+        }
+
+        estado_json = json.dumps(estado)
+        
+        if clientes:
+            tareas = [asyncio.create_task(cliente.send(estado_json)) for cliente in clientes if cliente.open]
+            if tareas:
+                await asyncio.wait(tareas)
+
+        await asyncio.sleep(0.1)
+
+start_server = websockets.serve(manejar_cliente, server_ip, server_port)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_server)
+loop.create_task(actualizar_estado())
+loop.run_forever()
